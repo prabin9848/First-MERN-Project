fc78fb23f285abe31bdb5f296476e4815e88ef9b
Prabin Shrestha <ps6854133@gmail.com>
2026-08-19 23:14:40 +0545

    created the edit page and applied the editing deleting and saving function

--- Frontend/src/pages/NoteDetail.jsx
+++ Frontend/src/pages/NoteDetail.jsx
@@ -1,11 +1,121 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import toast from "react-hot-toast";
+import { LoaderIcon, ArrowLeftIcon, Trash2Icon } from "lucide-react";
+import api from "../lib/axios";
 
 const NoteDetail = () => {
-  return (
-    <div>
-      NoteDetail
-    </div>
-  )
-}
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
 
-export default NoteDetail
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.log("error in fetching note", error);
+        toast.error("Failed to fetch the notes");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [id]);
+
+  const handleDelete = async() => {
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`)
+      toast.success("Note deleted");
+      navigate("/")
+    } catch (error) {
+      console.log("Error deleting the note:",error)
+      toast.error("Failed to delete note");
+    }
+  };
+  const handleSave = async() => {
+    if(!note.title)
+    try {
+      await api.post(`/notes/${id}`)
+      toast.success("Note saved");
+      navigate("/")  
+    } catch (error) {
+      console.log("Error saving the note: ",error)
+      toast.error("Failed to save the note")
+    }
+  };
+  console.log(note);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin"></LoaderIcon>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to={"/"} className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to Notes
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="btn btn-error btn-outline"
+            >
+              <Trash2Icon className="h-5 w-5" />
+              Delete Note
+            </button>
+          </div>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Note title"
+                  className="input input-bordered"
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+              </div>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">
+                    Content
+                  </span>
+                </label>
+                <textarea
+                placeholder="Write your note here..."
+                className="textarea textarea-bordered h-32"
+                value={note.content} 
+                onChange={(e) => setNote({ ...note, content: e.target.value})}
+                />
+              </div>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" disabled={saving} onClick={handleSave}>
+                  {saving ? "Saving...": "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteDetail;
